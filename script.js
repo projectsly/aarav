6f745bc6031b1307843a00bce5ac43edc991c014
aarav-saini <aaravsainidev@gmail.com>
2025-10-26 21:48:31 +0530

    Update about and stack, add auto age calculation (m too lazy)

--- script.js
+++ script.js
@@ -332,4 +332,17 @@ spiralGalaxy.addEventListener("mouseleave", () => {
     spiralGalaxy.style.scale = "1";
 });
 
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+document.getElementById("age").innerText = getAge("2011-11-21");
+
 startup();
